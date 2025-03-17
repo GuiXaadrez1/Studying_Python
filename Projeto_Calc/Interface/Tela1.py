116c6eab7c3471f7b6a7fd0fc9f6b4645af3ac11
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-03-17 07:26:45 -0300

    Projeto calculadora da minha namorada

--- Projeto_Calc/Interface/Tela1.py
+++ Projeto_Calc/Interface/Tela1.py
@@ -31,10 +31,27 @@ class Tela_Calc:
         
     def widgets_frame2(self):
         bt1 = ttk.Button(self.frame2,text='C')
-        bt1.place(relx=0.01, rely=0.02, relwidth=0.2, relheight=0.15)
-        bt2 = ttk.Button(self.frame2,text='M1')
-        bt2.place(relx=0.21, rely=0.02, relwidth=0.2, relheight=0.15)
-        pass    
+        bt1.place(relx=0.04, rely=0.02, relwidth=0.2, relheight=0.2)
+        bt2 = ttk.Button(self.frame2,text='CE')
+        bt2.place(relx=0.25, rely=0.02, relwidth=0.2, relheight=0.2)
+        bt3 = ttk.Button(self.frame2,text='⌫')
+        bt3.place(relx=0.77, rely=0.02, relwidth=0.2, relheight=0.2)
+        bt4 = ttk.Button(self.frame2,text='/')
+        bt4.place(relx=0.77, rely=0.22, relwidth=0.2, relheight=0.2)
+        bt5 = ttk.Button(self.frame2,text='*')
+        bt5.place(relx=0.55, rely=0.22, relwidth=0.2, relheight=0.2)
+        bt6 = ttk.Button(self.frame2,text='+')              
+        bt6.place(relx=0.77, rely=0.42, relwidth=0.2, relheight=0.39)
+        bt7 = ttk.Button(self.frame2,text='-')              
+        bt7.place(relx=0.55, rely=0.42, relwidth=0.2, relheight=0.2)
+        bt8 = ttk.Button(self.frame2,text='=')              
+        bt8.place(relx=0.55, rely=0.62, relwidth=0.2, relheight=0.2)
+        bt9 = ttk.Button(self.frame2,text=',')              
+        bt9.place(relx=0.55, rely=0.82, relwidth=0.2, relheight=0.15)
+        bt10 = ttk.Button(self.frame2,text="\U00002764")              
+        bt10.place(relx=0.55, rely=0.02, relwidth=0.2, relheight=0.2)  
+        bt11 = ttk.Button(self.frame2,text="\U00002764")              
+        bt11.place(relx=0.77, rely=0.82, relwidth=0.2, relheight=0.15)  
         
         
          
