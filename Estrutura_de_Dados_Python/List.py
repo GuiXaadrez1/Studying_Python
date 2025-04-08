0449f359f559a5ed4d292906aaf437ad2ee3c724
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-04-08 09:58:29 -0300

    Reforçando estrutura de dados em python

---
+++ Estrutura_de_Dados_Python/List.py
@@ -0,0 +1,18 @@
+# Vamos nos aperfeiçoar sobre estrutura de dados com python
+# O objetivo é reaprender e reforçar o conteudo
+
+
+# Criando uma lista
+
+lista = ['a','b','c','d','e'] # lista de strings
+print(type(lista)) # <class 'list'>
+
+# puxando o elemento de index 4, com o slice
+
+print(lista[4]) #saida e 
+
+print(lista[-2])# saida d o penultimo
+
+#for i in lista:
+#    print(i+' ',end='')    
+
---
+++ Estrutura_de_Dados_Python/Tuple.py
@@ -0,0 +1,7 @@
+# Vamos reaprender e reforçar o conteudo a respeito da tupla
+
+
+# criando uma tupla  de números
+tupla = (1,2,3,4,5)
+
+print(type(tupla)) # <class 'tuple'
