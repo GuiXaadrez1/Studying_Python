c01d0f9e0f019a2fb842e69ec1f08de34e02dfb0
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-04-07 23:07:25 -0300

    Aprendendo python

--- MPI_PYTHON/mpi_python.py
+++ MPI_PYTHON/mpi_python.py
@@ -1,25 +1,68 @@
 from mpi4py import MPI 
+import os
 
-'''
- quantidade de ranks não é definida dentro do seu código Python.
+def read_txt(nome: str):
+    with open(nome, 'r') as f:
+        linhas = f.readlines()
+    numeros = [int(l.strip()) for l in linhas]
+    return numeros
 
- Ela é definida na hora de executar o script, usando o comando mpiexec ou mpirun,
- com o parâmetro -n (ou --np).
-'''
+def dist_sum(comm, reader):
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    if rank == 0:
+        numeros = reader
+        num_workers = size - 1
+
+        while len(numeros) > 1:
+            workers_in_use = min(num_workers, len(numeros)//2)
+
+            for worker_id in range(1, workers_in_use + 1):
+                n1 = numeros.pop(0)
+                n2 = numeros.pop(0)
+                comm.send((n1, n2), dest=worker_id, tag=11)
+            
+            for worker_id in range(1, workers_in_use + 1):
+                soma = comm.recv(source=worker_id, tag=22)
+                numeros.append(soma)
+
+        for worker_id in range(1, size):
+            comm.send(None, dest=worker_id, tag=99)
+
+        print(f"Resultado final da soma: {numeros[0]}")
+
+    else:
+        while True:
+            status = MPI.Status()
+            dados = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+            if status.Get_tag() == 99:
+                break
+
+            n1, n2 = dados
+            resultado = n1 + n2
+            comm.send(resultado, dest=0, tag=22)
+
 
 def main():
-    # definindo o comunicador principal
-    comm = MPI.COMM_WORLD # inclui todos os processos definidos pelo usuário numa aplicação MPI 
-    total_process = comm.Get_size()# retorna o número total de processos no comunicador
-    
-    rank = comm.Get_rank() # adquire a quantidade de ranks no comunicador
+    comm = MPI.COMM_WORLD
+    total_process = comm.Get_size()
+    rank = comm.Get_rank()
 
-    if MPI.COMM_WORLD.Get_size() < 4:
+    if total_process < 4 and rank == 0:
         print("Poucos processos! Recomendo usar pelo menos 4.")
-         
-    return f'Existem {total_process} raks no comunicador, esse e o rank de id(processo): {rank}'
+    
+    if rank == 0:
+        caminho = os.path.join(os.getcwd(), 'somarnum.txt')
+        
+        reader = read_txt(caminho)
+    else:
+        reader = None
+    
+    dist_sum(comm, reader)
+
+    return f'Existem {total_process} ranks no comunicador, este é o rank de id(processo): {rank}'
 
 if __name__ == "__main__":
-
     test = main()
     print(test)
