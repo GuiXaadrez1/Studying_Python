ba871280a17e737399ba5dc1ace6efdd235a4c69
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-04-26 19:01:06 -0300

    Atualizando anotações gerais de estudos, trabalho de MPI e game cartiar

---
+++ MPI_PYTHON/mpi_reader.py
@@ -0,0 +1,22 @@
+'''
+Utilizando Python com MPI construir um contator de palavras do texto contido
+no link abaixo.
+
+https://www.gutenberg.org/cache/epub/55752/pg55752.txt
+
+A execução deve ser feita com 1, 2, 4, 8 , 12 processos e deverá ser medido a
+eficiência e speedup.
+
+A saída do programa deve ser a uma lista txt com a contagem de palavras onde em
+cada linha deverá conter uma palavra e o seu total:
+
+feliz 100
+feio 2
+viver 142
+
+O programa deve também informar as 10 palavras mais comuns no texto.
+
+O Aluno deve enviar na atividade a lista de contagem de palavras, a lista da 10 palavras
+mais frequentes, relatório com o speedupe enficiência, e o código-fonte do programa.
+'''
+
---
+++ game_lairs.py
@@ -0,0 +1,7 @@
+import random 
+
+tables = ['Aces_table','Kings_table','Queen_table']
+
+sort = random.choice(tables)
+
+print(sort)
